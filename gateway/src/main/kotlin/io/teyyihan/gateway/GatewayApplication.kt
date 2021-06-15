9e4c7f85ae934e4eac44007d05e6a04cb991b06a
teyyihan.aksu <teyyihan.aksu@trendyol.com>
2021-06-15 23:01:27 +0300

    Initial commit

---
+++ gateway/src/main/kotlin/io/teyyihan/gateway/GatewayApplication.kt
@@ -0,0 +1,11 @@
+package io.teyyihan.gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatewayApplication
+
+fun main(args: Array<String>) {
+    runApplication<GatewayApplication>(*args)
+}
---
+++ gateway/src/test/kotlin/io/teyyihan/gateway/GatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.teyyihan.gateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
