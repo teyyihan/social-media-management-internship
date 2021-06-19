b2d7431dd19ba2d83951bcb8a325c1bef47dbec2
teyyihan.aksu <teyyihan.aksu@trendyol.com>
2021-06-19 18:45:48 +0300

    Add kafka and listener

--- image-creator/kafka_listener.py
+++ image-creator/kafka_listener.py
@@ -1,3 +1,21 @@
 from config import config
-from json import loads
+from json import loads, dumps
+from kafka import KafkaConsumer, KafkaProducer
 
+consumer = KafkaConsumer(
+    *['instagram.post.image', 'twitter.post.image'],
+    bootstrap_servers=['localhost:9092'],
+    enable_auto_commit=True,
+    value_deserializer=lambda x: loads(x.decode('utf-8')))
+
+producer = KafkaProducer(bootstrap_servers=['localhost:9092'],
+                         value_serializer=lambda x:
+                         dumps(x).encode('utf-8'))
+
+# TODO: payload'da isteğin de bir id'si olmalı
+for message in consumer:
+    topic = message.topic
+    payload = message.value
+    # TODO: proccess and publish message with a image link, userId and isteğin id'si
+    returnValue = {}
+    producer.send(topic, value=returnValue)
